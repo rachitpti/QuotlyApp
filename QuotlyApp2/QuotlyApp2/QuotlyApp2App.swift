dc5d6f5244a6ca83ffb8aa160dee111684d925ab
Rachit <rdprajapati@icloud.com>
2024-01-20 15:31:16 -0800

    Add files via upload

---
+++ QuotlyApp2/QuotlyApp2/QuotlyApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  QuotlyApp2App.swift
+//  QuotlyApp2
+//
+//  Created by Rachit Prajapati on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct QuotlyApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
